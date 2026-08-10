f8c5e31ea6cd4c3f3f015605c90d82f05fcd680e
Faiz Arfi <faizarfi12@gamil.com>
2026-08-10 17:21:05 +0530

    initial project structure

---
+++ src/main/java/dev/faizarfi/utility/apierrorhelper/ApiErrorHelperApplication.java
@@ -0,0 +1,13 @@
+package dev.faizarfi.utility.apierrorhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiErrorHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiErrorHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/faizarfi/utility/apierrorhelper/ApiErrorHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.faizarfi.utility.apierrorhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiErrorHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
